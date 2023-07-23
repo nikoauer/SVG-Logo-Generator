0621fe427e65b199809f878a74dfaee897304882
Niko Auer <nikoauer26@gmail.com>
2023-07-23 12:15:02 +1000

    added questions to prompt

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        message: 'is this working?',
-        name: 'answer'
+        message: 'What letters do you want to be used for your logo?',
+        name: 'Title'
+    },
+    {
+        type: 'input',
+        message: 'What colour would you like your font?',
+        name: 'textColour'
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like?',
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'shape'
+
+    },
+    {
+        type: 'input',
+        message: 'What colour would you like your shape to be?',
+        name: 'shapeColour'
     }
 ]
 
 function init() {
     inquirer.prompt(questions)
-      .then((data) => { console.log(`${data.answer}`)
+      .then((data) => { console.log(`${data.shapeColour}`)
   
       })
       .catch((error) => {
