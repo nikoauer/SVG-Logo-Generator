0191cc352589153309b1856cdc116ae809e52f81
Niko Auer <nikoauer26@gmail.com>
2023-07-26 14:13:53 +1000

    fixed .then function call

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-const CLI = require('./library/cli');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const SVGLogo = require("./library/shapes");
 
-const cli = new CLI();
+function init (){
+inquirer
+  .prompt([
+  {
+    type: "input",
+    message: "What letters do you want to be used for your logo (maximum of 3 characters)?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What colour would you like your font?",
+    name: "textColour",
+  },
+  {
+    type: "list",
+    message: "What shape would you like?",
+    choices: ["Circle", "Triangle", "Square"],
+    name: "shape",
+  },
+  {
+    type: "input",
+    message: "What colour would you like your shape to be?",
+    name: "shapeColour",
+  },
+])
+.then((answers) => {
+  let SVGproperties = new SVGLogo (
+    answers.title,
+    answers.textColour,
+    answers.shape,
+    answers.shapeColour
+    ) 
+      generateLogo(SVGproperties)
+  })
+.catch((error) => {
+      console.error("Error Occured:", error);
+    });
+}
 
-cli.run();
+init();
+
+function generateLogo(SVGproperties) {
+ return console.log(SVGproperties.shape)
+}
