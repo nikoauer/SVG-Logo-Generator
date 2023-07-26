f325833834bee05323feef48c1072e5ade4fb055
Niko Auer <nikoauer26@gmail.com>
2023-07-26 17:15:38 +1000

    added constructor to inquirer data in generatelogo

--- library/generateLogo.js
+++ library/generateLogo.js
@@ -3,21 +3,28 @@ const SVGLogo = require("./shapes")
 
 function generateLogo(data){
 
+    let newLogo = new SVGLogo (
+        data.title,
+        data.textColour,
+        data.shape,
+        data.shapeColour
+    )
+
     //create the svg elements with a width of 300 and height of 200
     let svgMarkup = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
 
     //we add a centered text element and its colour
-    svgMarkup += `<text x="50" y="50" fill="${data.textColour}" text-anchor="middle" dominant-baseline="central" font-size="20">${data.title}</text>`;
+    svgMarkup += `<text x="50" y="50" fill="${newLogo.textColour}" text-anchor="middle" dominant-baseline="central" font-size="20">${newLogo.title}</text>`;
 
     //this cycles through the conditionals depending on which shape was chosen it will select the correct svg code
-    if (data.shape === "Circle"){
-        svgMarkup += `<circle cx="50" cy="50" r="45" fill="${data.shapeColour}" />`;
+    if (newLogo.shape === "Circle"){
+        svgMarkup += `<circle cx="50" cy="50" r="45" fill="${newLogo.shapeColour}" />`;
     }
-    if (data.shape === "Triangle"){
-        svgMarkup += `<polygon points="50,5 5,95 95,95" fill="${data.shapeColour}" />`;
+    if (newLogo.shape === "Triangle"){
+        svgMarkup += `<polygon points="50,5 5,95 95,95" fill="${newLogo.shapeColour}" />`;
     }
-    if (data.shape === "Square"){
-        svgMarkup += `<rect width="100" height="100" fill="${data.shapeColour}" />`;
+    if (newLogo.shape === "Square"){
+        svgMarkup += `<rect width="100" height="100" fill="${newLogo.shapeColour}" />`;
     };
     // closing svg statement 
     svgMarkup += "</svg>";
