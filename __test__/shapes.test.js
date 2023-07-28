9dff0afd65350d175fdb3b523481893e2ea11b4a
Niko Auer <nikoauer26@gmail.com>
2023-07-28 12:12:48 +1000

    added tests & change output svg file location

--- __test__/shapes.test.js
+++ __test__/shapes.test.js
@@ -1,14 +1,38 @@
-const Shapes = require('../library/shapes');
+const SVGLogo = require('../library/shapes');
 
-let title = "asd";
-let shape = "Circle";
-let textColour = "green";
-let shaperColour = "black";
-
-let logo = new Shapes(title, textColour, shape, shaperColour)
-
+// Testing for Circle shape
 test('testing for circle object', () => {
-    expect(logo.render()).toBe(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" fill="black" /><text x="50" y="50" fill="green" text-anchor="middle" dominant-baseline="central" font-size="20">asd</text></svg>`)
+  let title = "XYX";
+  let shape = "Circle";
+  let textColour = "green";
+  let shapeColour = "black";
+
+  let logo = new SVGLogo(title, textColour, shape, shapeColour);
+
+  expect(logo.render()).toEqual(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" fill="black" /><text x="50" y="50" fill="green" text-anchor="middle" dominant-baseline="central" font-size="20">XYX</text></svg>`);
 });
 
-test('Testign for title', () => expect(logo.title).toBe(title));
+
+// Test for Triangle shape
+test('testing for triangle object', () => {
+    let title = "ABC";
+    let shape = "Triangle";
+    let textColour = "blue";
+    let shapeColour = "red";
+  
+    let logo = new SVGLogo(title, textColour, shape, shapeColour);
+  
+    expect(logo.render()).toEqual(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="50,5 5,95 95,95" fill="red" /><text x="50" y="50" fill="blue" text-anchor="middle" dominant-baseline="central" font-size="20">ABC</text></svg>`);
+  });
+
+  //Test for Square shape
+  test('testing for square object', () => {
+    let title = "123";
+    let shape = "Square";
+    let textColour = "yellow";
+    let shapeColour = "purple";
+  
+    let logo = new SVGLogo(title, textColour, shape, shapeColour);
+  
+    expect(logo.render()).toEqual(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100" height="100" fill="purple" /><text x="50" y="50" fill="yellow" text-anchor="middle" dominant-baseline="central" font-size="20">123</text></svg>`);
+  });
--- library/generateLogo.js
+++ library/generateLogo.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const SVGLogo = require("./shapes")
+const path = require("path")
 
 //then the users answers will be saved using the SVG logo constructor
 function generateLogo(data){
@@ -10,13 +11,18 @@ function generateLogo(data){
         data.shape,
         data.shapeColour
         )
-
-
+    
+        const folderPath = "./output";
+        const fileName = "logo.svg";
+        const filePath = path.join(folderPath, fileName);
     //this writes the svg file and notifies the users it has been generated
-    fs.writeFile('logo.svg', newLogo.render(), (err) => {
-        if (err) throw err;
+    fs.writeFile(filePath, newLogo.render(), (err) => {
+        if (err){
+            console.log("Error generated:", err);
+        } else {
         console.log('Logo.svg has been generated!');
-      });
+      }
+    });
 }
 
 module.exports = { generateLogo }
